40ee16e752fecbb575b426bd13b98bd9b7ab58fa
NWuSunset <NathanielWuPersonal@proton.me>
2026-05-15 21:43:09 +0000

    Build camke, current works

--- lidar_node/lidar_node.cc
+++ lidar_node/lidar_node.cc
@@ -1,44 +1,53 @@
 #include <gz/msgs/twist.pb.h>
-#include <gz/msg/laserscan.pb.h>
+#include <gz/msgs/laserscan.pb.h>
 #include <gz/transport/Node.hh>
 
-std::string topic_pub = "/cmd_vel"; //topic to puublish to
+
+std::string topic_pub = "/cmd_vel";   //publish to this topic
 gz::transport::Node node;
-auto pub = node.Advertise<gz::msgs::Twist>(topic_pub)
+auto pub = node.Advertise<gz::msgs::Twist>(topic_pub);
 
-void cb(const gz::msgs::LaserScan &_msg) {
-    gz::msgs::Twist data;
+//////////////////////////////////////////////////
+/// \brief Function called each time a topic update is received.
+void cb(const gz::msgs::LaserScan &_msg)
+{
+  gz::msgs::Twist data;
 
-    bool allMore = true;
-    for (int i = 0; i < _msg.ranges.size(); i++) {
-        if (_msg.ranges(i) < 1.0) {
-            allMore = false;
-            break;
-        }
-    }
-    if (allMore) //if all bigger than one (rays bigger than 1.0, robot move forward)
+  bool allMore = true;
+  for (int i = 0; i < _msg.ranges_size(); i++)
+  {
+    if (_msg.ranges(i) < 1.0) 
     {
-        data.mutable_linear()->set_x(0.5);
-        data.mutable_angular()->set_z(0.0);
+      allMore = false;
+      break;
     }
-    else
-    {
-        data.mutable_linear()->set_x(0.0);
-        data.mutable_angular()->set_z(0.5);
-    }
-    pub.Publish(data);
+  }
+  if (allMore) //if all bigger than one
+  {
+    data.mutable_linear()->set_x(0.5);
+    data.mutable_angular()->set_z(0.0);
+  }
+  else
+  {
+    data.mutable_linear()->set_x(0.0);
+    data.mutable_angular()->set_z(0.5);
+  }
+  pub.Publish(data);
 }
 
-int main(int argc, char **argv) {
-    std::string topic_sub = "/lidar";
-    //subscribe to topic by registering callback
-    if (!node.Subscribe(topic_sub, cb)) {
-        std::cerr << "Error subscribing to topic [" << topic_sub << "]" << std::endl;
-        return -1;
-    }
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  std::string topic_sub = "/lidar";   // subscribe to this topic
+  // Subscribe to a topic by registering a callback.
+  if (!node.Subscribe(topic_sub, cb))
+  {
+    std::cerr << "Error subscribing to topic [" << topic_sub << "]" << std::endl;
+    return -1;
+  }
 
-    // Zzzzzz.
-    gz::transport::waitForShutdown();
+  // Zzzzzz.
+  gz::transport::waitForShutdown();
 
-    return 0;
+  return 0;
 }
