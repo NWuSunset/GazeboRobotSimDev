c53c2dc5d7902e0ddbc56092b4bc57daf65041f0
Nathaniel Wu <136409307+NWuSunset@users.noreply.github.com>
2026-06-04 13:08:11 -0700

    Delete build directory

--- build/ros_gz_bringup/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/workspaces/GazeboRobotSimDev/install/ros_gz_application/share/ros_gz_application/package.sh"
-. "/workspaces/GazeboRobotSimDev/install/ros_gz_description/share/ros_gz_description/package.sh"
-. "/workspaces/GazeboRobotSimDev/install/ros_gz_gazebo/share/ros_gz_gazebo/package.sh"
